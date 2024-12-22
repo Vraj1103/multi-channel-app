08e589e03c5acec00add045c2761cd0d5775d26b
Vraj1103 <svraj073@gmail.com>
2024-12-22 13:40:49 +0530

    db bug fix

--- app/services/database.py
+++ app/services/database.py
@@ -8,8 +8,10 @@ if client:
     print("Connected to MongoDB")
 db = client.get_database("slack_bot")
 db_sms = client.get_database("sms_bot")
+db_whatsapp = client.get_database("whatsapp_bot")
 slack_messages_collection = db.get_collection("messages")
 sms_messages_collection = db_sms.get_collection("messages")
+whatsapp_messages_collection = db.get_collection("messages")
 
 def save_message(message_data,app:str):
     if app == "slack":
@@ -28,6 +30,14 @@ def save_message(message_data,app:str):
                 print("No message data to save")
         except Exception as e:
             print(f"Error saving message: {e}")
+    elif app == "whatsapp":
+        try:
+            if message_data:
+                whatsapp_messages_collection.insert_one(message_data)
+            else:
+                print("No message data to save")
+        except Exception as e:
+            print(f"Error saving message: {e}")
 
 
 def get_messages(app:str):
@@ -37,6 +47,34 @@ def get_messages(app:str):
             # Fetch all documents from the collection
 
             messages_cursor = slack_messages_collection.find({}, {"timestamp": 1, "text": 1, "user": 1, "_id": 0})
+
+            # Convert cursor to a list
+            messages = list(messages_cursor)
+            # messages_json = json.dumps(messages)
+            return messages
+        except Exception as e:
+            print(f"Error fetching messages: {e}")
+            return []
+    elif app == "sms":
+        try:
+            # print("In get_messages")
+            # Fetch all documents from the collection
+
+            messages_cursor = sms_messages_collection.find({}, {"timestamp": 1, "text": 1, "user": 1, "_id": 0})
+
+            # Convert cursor to a list
+            messages = list(messages_cursor)
+            # messages_json = json.dumps(messages)
+            return messages
+        except Exception as e:
+            print(f"Error fetching messages: {e}")
+            return []
+    elif app == "whatsapp":
+        try:
+            # print("In get_messages")
+            # Fetch all documents from the collection
+
+            messages_cursor = whatsapp_messages_collection.find({}, {"timestamp": 1, "text": 1, "user": 1, "_id": 0})
 
             # Convert cursor to a list
             messages = list(messages_cursor)
